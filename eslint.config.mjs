d2226de93b1ab09aeda01df2cf63662d38c135b0
Kushagra Brandy <kushagrabrandy.fi@gmail.com>
2026-03-05 05:11:51 +0200

    a

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,18 +9,16 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-/** @type {import('eslint').Linter.Config[]} */
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    // Custom rules and overrides
     rules: {
       "@typescript-eslint/no-unused-vars": "warn",
       "react/no-unescaped-entities": "off",
     },
   },
   {
-    ignores: [".next/*", "out/*", "build/*", "public/*"],
+    ignores: [".next/**", "node_modules/**"],
   },
 ];
 
